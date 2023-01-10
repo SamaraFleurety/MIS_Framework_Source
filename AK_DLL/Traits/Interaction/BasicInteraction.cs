5b5dd3e781c08b53f0855fdaee9d5ddccf2fb5f6
neutronest <neutronest@gmail.com>
2023-01-10 19:35:49 +1100

    add Interaction Skeleton Code

---
+++ AK_DLL/Traits/Interaction/BasicInteraction.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AK_DLL.Traits.Interaction
+{
+    public class BasicInteraction : InteractionWorker
+    {
+        public override float RandomSelectionWeight(Pawn initiator, Pawn recipient)
+        {
+            if (AK_Tool.GetArkNightsHeDiffByPawn(initiator) is null || AK_Tool.GetArkNightsHeDiffByPawn(recipient) is null)
+            {
+                return 0f;
+            }
+            return 1f;
+        }
+
+    }
+}

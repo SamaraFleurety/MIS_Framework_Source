676738c84ed4f75937143cbee9a16f630ab72688
neutronest <neutronest@gmail.com>
2023-01-13 01:55:10 +1100

    Add Special Interaction Skeleton

---
+++ AK_DLL/Def/SpecialInteractionDef.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using UnityEngine;
+using AK_DLL.Traits.Interaction;
+using System.Collections.Generic;
+
+namespace AK_DLL
+{
+    public class SpecialInteractionDef : InteractionDef
+    {
+        public List<string> involvedPawns = new List<string>();
+    }
+}
---
+++ AK_DLL/Traits/Interaction/SpecialInteraction.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+using System.Linq;
+
+
+namespace AK_DLL.Traits.Interaction
+{
+    public class SpecialInteraction : InteractionWorker
+    {
+        public override float RandomSelectionWeight(Pawn initiator, Pawn recipient)
+        {
+            var involvedPawns = (this.interaction as SpecialInteractionDef).involvedPawns;
+            var realPawnNames = from interactionPawnName in involvedPawns select interactionPawnName.Split('_').Last();
+
+            var initiatorDoc = initiator.GetDoc();
+            var recipientDoc = recipient.GetDoc();
+            if (initiatorDoc is null || recipientDoc is null)
+            {
+                return 0f;
+            }
+            if (!realPawnNames.Contains(initiatorDoc.defName) || !realPawnNames.Contains(recipientDoc.defName))
+            {
+                return 0f;
+            }
+            return 1f;
+        }
+    }
+}

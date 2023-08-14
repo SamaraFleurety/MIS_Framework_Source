06bf89eb00993cfcdb442a76e8c2d7f42dc0f092
MoonShine <ljhmyw@foxmail.com>
2023-08-15 01:43:26 +0800

    矿石病相关全部移到工业那边去

--- AK_DLL/Def/OperatorDef/HediffStat.cs
+++ AK_DLL/Def/OperatorDef/HediffStat.cs
@@ -14,8 +14,6 @@ namespace AK_DLL
         public float serverity = 1f;  //这个是招募时给的状态的严重度，和矿石病随机无关
         //以下是矿石病增加hediff时的加权随机
         public int randWeight = 1;
-        public int randWorseMin = 0;
-        public int randWorseMax = 1;
-        public List<HediffStat> complication;
+        //public int randWorse = 1;
     }
 }

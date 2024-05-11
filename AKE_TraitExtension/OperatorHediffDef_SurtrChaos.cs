699a2c56a6fb4e0b73f067f410c8f870c7922913
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-05-12 01:40:03 +0800

    随机特性框架

---
+++ AKE_TraitExtension/OperatorHediffDef_SurtrChaos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AKE_TraitExtension
+{
+    public class OperatorHediffDef_SurtrChaos : Def
+    {
+        public int order;
+        public float AddHediffChance = 0.2f;
+        public HediffDef HediffToAdd;
+    }
+}

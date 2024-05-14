ed943ca78714718fb7c55a3bccab599ed50caec6
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-05-15 00:27:58 +0800

    +RandStatsDef

---
+++ AKE_OperatorExtension/TRP/RandStatsDef.cs
@@ -0,0 +1,18 @@
+﻿using AK_DLL;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AKE_TraitExtension
+{
+    public class RandStatsDef : Def
+    {
+        public SkillDef skill;
+        public int level;
+        public Passion fireLevel;
+    }
+}

f769cc2a6323a3ecedb022e162631acd501c313e
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-05-16 21:38:46 +0800

    随机特性框架更新(Almost Done)

--- AKE_OperatorExtension/HCP/HCP_RandAddTrait.cs
+++ AKE_OperatorExtension/HCP/HCP_RandAddTrait.cs
@@ -15,8 +15,7 @@ namespace AKE_OperatorExtension
     {
         public int interval = 1;
         public TimeToTick intervalUnit = TimeToTick.day;
-        public List<Trait> traitInatePlusMaxPro = new List<Trait>();
-
+        public List<TraitAndDegree> TraitSets = new List<TraitAndDegree>();
         public HCP_RandStats()
         {
             this.compClass = typeof(HC_RandStats);
@@ -26,132 +25,89 @@ namespace AKE_OperatorExtension
     public class HC_RandStats : HediffComp
     {
         #region
-        //public HCP_RandStats Props => (HCP_RandStats)this.props;
-        public HCP_RandStats Props
-        {
-            get { return (HCP_RandStats)this.props; }
-        }
+        public HCP_RandStats Props => (HCP_RandStats)this.props;
         //保存随机修正模板的特性List
-        private List<Trait> TraitStats
-        {
-            get { return this.Props.traitInatePlusMaxPro; }
-        }
-        private List<RandStatsDef> Randskills;
-        private int index = 0;
+        public List<TraitAndDegree> TraitSets => this.Props.TraitSets;
+        //储存TraitSet的索引
+        private HashSet<int> UsedIndex = new HashSet<int>();
+
+        private static int index = 0;
         private int tick = 0;
-        private int tick_bingchilling = 0;
-        private static bool hasSkill = false;
-        /*private List<HediffStat> HediffInateStats
-        {
-            get { return this.Props.hediffInatePlusMaxPro; }
-        }*/
-        //检测条件，冰淇淋增益3天
-        private int TimerInterval_Bingchilling
-        {
-            get { return 3 * this.Props.interval * (int)this.Props.intervalUnit; }
-        }
+        //检测条件为多少天
+        private int TimerInterval_days(int value) => value * this.Props.interval * (int)this.Props.intervalUnit;
 
-        private int TimerInterval_Phase1
-        {
-            get { return 10 * this.Props.interval * (int)this.Props.intervalUnit; }
-        }
-        private int TimerInterval_Phase2
-        {
-            get { return 15 * (this.Props.interval) * (int)this.Props.intervalUnit; }
-        }
-        private int TimerInterval_Phase3
-        {
-            get { return 20 * (this.Props.interval) * (int)this.Props.intervalUnit; }
-        }
+        public static bool hascurrentTrait = false;
+        //读取干员身份证，非空
+        private string OperatorID => this.Pawn.GetDoc()?.operatorID;
+        private string GetOperatorTraitDef => $"AK_Trait_{OperatorID}";
+        private string ThingdefName(int i) => AK_Tool.GetThingdefNameFrom((OperatorID + "Chaos" + i), "AKE", "Trait");
         #endregion
-
-        private string GetID()
-        {
-            return this.Pawn.GetDoc()?.operatorID;
-        }
-
-        private string GetTraitDef()
-        {
-            return $"AK_Trait_{GetID()}";
-        }
-
-        public void AddTrait()
-        {
-            foreach (Trait TraitDef in this.TraitStats)
-            {
-
-            }
-            for(int i=0;i<this.TraitStats.Count;i++)
-            this.Pawn.story.traits.GainTrait(TraitStats[i], false);
-        }
-
         //检查小人身上还有没有特定TraitDef
-        public bool HasTrait(string XMLdefName)
+        private bool HasOperatorTraitDef(string XMLdefName)
         {
             for (int i = 0; i < this.Pawn.story.traits.allTraits.Count; i++)
             {
                 if (this.Pawn.story.traits.allTraits[i].def.defName == DefDatabase<TraitDef>.GetNamed(XMLdefName).defName)
-                {
                     return true;
-                }
             }
             return false;
         }
-
-        //特性修正(-6,16)
+        private Trait HC_GetCurrentTrait()
+        {
+            if (this.Pawn == null) return null;
+            if (TraitSets == null) return null;
+            for (int i = 0; i < this.Pawn.story.traits.allTraits.Count; i++)
+            {
+                for (int j = 0; i < TraitSets.Count; j++)
+                {
+                    if (this.Pawn.story.traits.HasTrait(TraitSets[j].def))
+                    {
+                        index = j;
+                        return this.Pawn.story.traits.allTraits[i];
+                    }
+                }
+            }
+            return null;
+        }
+        private void HC_AddTrait()
+        {
+            if (this.Pawn == null) return;
+            if (TraitSets == null) return;
+            /*这部分今天懒得写先鸽了...记得使用index和UsedIndex计数检索
+            foreach (TraitAndDegree TraitAndDegree in this.TraitSets)
+            {
+                TraitDegreeData data = TraitAndDegree.traitDef.degreeDatas[TraitAndDegree.degree];
+                if (data.skillGains != null)
+                {
+                    index++;
+                    this.Pawn.story.traits.GainTrait(new Trait(TraitAndDegree.traitDef, TraitAndDegree.degree));
+                }
+            }*/
+            hascurrentTrait = true;
+        }
+        private void HC_RemoveTrait()
+        {
+            if (this.Pawn == null) return;
+            if (TraitSets == null) return;
+            if (hascurrentTrait)
+            {
+                this.Pawn.story.traits.RemoveTrait(HC_GetCurrentTrait());
+                hascurrentTrait = false;
+            }
+        }
         public override void CompPostTick(ref float severityAdjustment)
         {
             ++tick;
-            if (tick >= TimerInterval_Phase1)
+            if (tick >= TimerInterval_days(10))
             {
-                if (!HasTrait(GetTraitDef())) { parent.comps.Remove(this); return; }
+                if (!HasOperatorTraitDef(GetOperatorTraitDef)) { parent.comps.Remove(this); return; }
+                HC_RemoveTrait();
                 tick = 0;
-                /*foreach (RandStatsDef skillDef in this.Randskills)
-                {
-                    index++;
-                    if (index == 7)
-                    { SetSkill(skillDef, 16); continue; }
-                    RandSkill(skillDef,-6,16);
-                }*/
+                HC_AddTrait();
                 index = 0;
                 string translatedMessage = TranslatorFormattedStringExtensions.Translate("Phase1_SuccessMessage");
                 MoteMaker.ThrowText(this.Pawn.PositionHeld.ToVector3(), this.Pawn.MapHeld, translatedMessage, 2f);
             }
-        }
-
-        private void RandSkill(RandStatsDef skillDef, int min, int max)
-        {
-            int randomAdjustment = RandomLevelAdjustment(min, max);
-            int adjustedLevel = skillDef.level + randomAdjustment;
-            adjustedLevel = Math.Min(adjustedLevel, 20);
-            SkillRecord skill = new SkillRecord(this.Pawn, skillDef.skill)
-            {
-                passion = skillDef.fireLevel,
-                Level = adjustedLevel
-            };
-            this.Pawn.skills.skills.Add(skill);
-        }
-
-        private void SetSkill(RandStatsDef skillDef, int value)
-        {
-            int adjustedLevel = value;
-            SkillRecord skill = new SkillRecord(this.Pawn, skillDef.skill)
-            {
-                passion = skillDef.fireLevel,
-                Level = adjustedLevel
-            };
-            this.Pawn.skills.skills.Add(skill);
-        }
-        //调整小人的火有刷属性嫌疑 先弃用
-        /*private byte RandomPassionAdjustment(int min,int max)
-        {
-            byte RandomPassion = (byte)new System.Random().Next(min, max);
-            return RandomPassion; 
-        }*/
-        private int RandomLevelAdjustment(int min, int max)
-        {
-            int RandomLevel = new System.Random().Next(min, max);
-            return RandomLevel;
         }
     }
 }

491cebeffd279dd8b7cb7821ca75115e60fa6295
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-05-16 21:30:53 +0800

    Revert "更新随机特性(Almost Done)"
    This reverts commit 6c26f99f946f84ce8adc7caecd9307437a21554a.

--- AKE_OperatorExtension/HCP/HCP_RandAddHediff.cs
+++ AKE_OperatorExtension/HCP/HCP_RandAddHediff.cs
@@ -1,113 +1,85 @@
-﻿using RimWorld;
+﻿using AK_DLL;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
-using AK_DLL;
-using UnityEngine;
-using AKE_TraitExtension;
-using System.Diagnostics;
 
 namespace AKE_OperatorExtension
 {
-    public class HCP_RandStats : HediffCompProperties
+    /// <summary>
+    /// 随机附加Hediff。<para></para>先判定豁免概率(优先使用函数，没有就使用固定概率)
+    /// 若豁免失败就会从hediffStatsSet里面使用加权随机随一个
+    /// </summary>
+    public class HCP_RandAddHediff : HediffCompProperties
     {
         public int interval = 1;
-        public TimeToTick intervalUnit = TimeToTick.day;
-        public List<TraitAndDegree> TraitSets = new List<TraitAndDegree>();
-        public HCP_RandStats()
+        public TimeToTick intervalUnit = TimeToTick.hour;
+        //public Type saveChanceByFunc = null; //优先使用概率函数。如果函数为空就用下面静态值
+        public float saveChance = 0f; //有多少概率会无事发生。1就是100%。
+        public List<HediffStat> hediffStatsSet = new List<HediffStat>();
+
+        public HCP_RandAddHediff()
         {
-            this.compClass = typeof(HC_RandStats);
+            this.compClass = typeof(HC_RandAddHediff);
         }
     }
 
-    public class HC_RandStats : HediffComp
+    public class HC_RandAddHediff : HediffComp
     {
-        #region
-        public HCP_RandStats Props => (HCP_RandStats)this.props;
-        //保存随机修正模板的特性List
-        public List<TraitAndDegree> TraitSets => this.Props.TraitSets;
-        //储存TraitSet的索引
-        private HashSet<int> UsedIndex = new HashSet<int>();
+        public HCP_RandAddHediff Props => (HCP_RandAddHediff)this.props;
 
-        private static int index = 0;
         private int tick = 0;
-        //检测条件为多少天
-        private int TimerInterval_days(int value) => value * this.Props.interval * (int)this.Props.intervalUnit;
 
-        public static bool hascurrentTrait = false;
-        //读取干员身份证，非空
-        private string OperatorID => this.Pawn.GetDoc()?.operatorID;
-        private string GetOperatorTraitDef => $"AK_Trait_{OperatorID}";
-        private string ThingdefName(int i) => AK_Tool.GetThingdefNameFrom((OperatorID + "Chaos" + i), "AKE", "Trait");
-        #endregion
-        //检查小人身上还有没有特定TraitDef
-        private bool HasOperatorTraitDef(string XMLdefName)
+        private int[] weight;
+
+        private bool arrayCached = false;
+
+        //private int arraySum;
+        private List<HediffStat> HediffStats
         {
-            for (int i = 0; i < this.Pawn.story.traits.allTraits.Count; i++)
-            {
-                if (this.Pawn.story.traits.allTraits[i].def.defName == DefDatabase<TraitDef>.GetNamed(XMLdefName).defName)
-                    return true;
-            }
-            return false;
+            get { return this.Props.hediffStatsSet; }
         }
-        private Trait HC_GetCurrentTrait()
+
+        private int Interval
         {
-            if (this.Pawn == null) return null;
-            if (TraitSets == null) return null;
-            for (int i = 0; i < this.Pawn.story.traits.allTraits.Count; i++)
-            {
-                for (int j = 0; i < TraitSets.Count; j++)
-                {
-                    if (this.Pawn.story.traits.HasTrait(TraitSets[j].def))
-                    {
-                        index = j;
-                        return this.Pawn.story.traits.allTraits[i];
-                    }
-                }
-            }
-            return null;
+            get { return this.Props.interval * (int)this.Props.intervalUnit; }
         }
-        private void HC_AddTrait()
+
+        double SaveChance
         {
-            if (this.Pawn == null) return;
-            if (TraitSets == null) return;
-            /*这部分今天懒得写先鸽了...记得使用index和UsedIndex计数检索
-            foreach (TraitAndDegree TraitAndDegree in this.TraitSets)
+            get
             {
-                TraitDegreeData data = TraitAndDegree.traitDef.degreeDatas[TraitAndDegree.degree];
-                if (data.skillGains != null)
-                {
-                    index++;
-                    this.Pawn.story.traits.GainTrait(new Trait(TraitAndDegree.traitDef, TraitAndDegree.degree));
-                }
-            }*/
-            hascurrentTrait = true;
-        }
-        private void HC_RemoveTrait()
-        {
-            if (this.Pawn == null) return;
-            if (TraitSets == null) return;
-            if (hascurrentTrait)
-            {
-                this.Pawn.story.traits.RemoveTrait(HC_GetCurrentTrait());
-                hascurrentTrait = false;
+                return Props.saveChance;
             }
+        }
+        private void CaculateWeightArray()
+        {
+            this.weight = new int[this.HediffStats.Count];
+            this.weight[0] = this.HediffStats[0].randWeight;
+            for (int i = 1; i < this.HediffStats.Count; ++i)
+            {
+                this.weight[i] = this.weight[i - 1] + this.HediffStats[i].randWeight;
+            }
+            //arraySum = this.weight.Last();
+            this.arrayCached = true;
         }
         public override void CompPostTick(ref float severityAdjustment)
         {
             ++tick;
-            if (tick >= TimerInterval_days(10))
+            if (tick >= Interval)
             {
-                if (!HasOperatorTraitDef(GetOperatorTraitDef)) { parent.comps.Remove(this); return; }
-                HC_RemoveTrait();
+                if (!arrayCached)
+                {
+                    this.CaculateWeightArray();
+                }
                 tick = 0;
-                HC_AddTrait();
-                index = 0;
-                string translatedMessage = TranslatorFormattedStringExtensions.Translate("Phase1_SuccessMessage");
-                MoteMaker.ThrowText(this.Pawn.PositionHeld.ToVector3(), this.Pawn.MapHeld, translatedMessage, 2f);
+                if (UnityEngine.Random.Range(0f, 1f) <= SaveChance) return;
+
+                HediffStat hediff = HediffStats[AK_Tool.weightArrayRand(weight)];
+                AbilityEffect_AddHediff.AddHediff(this.Pawn, hediff.hediff, hediff.part, severity: hediff.serverity);
             }
         }
     }

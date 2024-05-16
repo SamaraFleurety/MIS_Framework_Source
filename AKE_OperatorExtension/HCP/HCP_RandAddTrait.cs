11df52ac3f6a07b25d894aec5767edc8b01fe343
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-05-17 01:44:35 +0800

    +HCP随机特性 (真的写完了)

--- AKE_OperatorExtension/HCP/HCP_RandAddTrait.cs
+++ AKE_OperatorExtension/HCP/HCP_RandAddTrait.cs
@@ -7,42 +7,44 @@ using System.Threading.Tasks;
 using Verse;
 using AK_DLL;
 using UnityEngine;
-using AKE_TraitExtension;
 
 namespace AKE_OperatorExtension
 {
     public class HCP_RandStats : HediffCompProperties
     {
-        public int interval = 1;
+        public int interval = 10;
+        public int positiveinterval = 3;
         public TimeToTick intervalUnit = TimeToTick.day;
+        public List<TraitAndDegree> positiveTraitSets = new List<TraitAndDegree>();
         public List<TraitAndDegree> TraitSets = new List<TraitAndDegree>();
         public HCP_RandStats()
         {
             this.compClass = typeof(HC_RandStats);
         }
     }
-
     public class HC_RandStats : HediffComp
     {
         #region
         public HCP_RandStats Props => (HCP_RandStats)this.props;
-        //保存随机修正模板的特性List
+        //保存随机修正模板 & 正面特性修正的List
         public List<TraitAndDegree> TraitSets => this.Props.TraitSets;
+        public List<TraitAndDegree> PositiveTraitSets => this.Props.positiveTraitSets;
         //储存TraitSet的索引
-        private HashSet<int> UsedIndex = new HashSet<int>();
+        //private HashSet<int> UsedIndex = new HashSet<int>();
+        private Dictionary<List<TraitAndDegree>, HashSet<int>> usedIndices = new Dictionary<List<TraitAndDegree>, HashSet<int>>();
 
         private static int index = 0;
         private int tick = 0;
         //检测条件为多少天
-        private int TimerInterval_days(int value) => value * this.Props.interval * (int)this.Props.intervalUnit;
-
-        public static bool hascurrentTrait = false;
+        private int TimerInterval_days => this.Props.interval * (int)this.Props.intervalUnit;
+        private int positiveTimerInterval_days => this.Props.positiveinterval * (int)this.Props.intervalUnit;
         //读取干员身份证，非空
         private string OperatorID => this.Pawn.GetDoc()?.operatorID;
         private string GetOperatorTraitDef => $"AK_Trait_{OperatorID}";
-        private string ThingdefName(int i) => AK_Tool.GetThingdefNameFrom((OperatorID + "Chaos" + i), "AKE", "Trait");
         #endregion
         //检查小人身上还有没有特定TraitDef
+        public static bool hascurrentTrait = false;
+        public static bool haseatbingchilling = false;
         private bool HasOperatorTraitDef(string XMLdefName)
         {
             for (int i = 0; i < this.Pawn.story.traits.allTraits.Count; i++)
@@ -52,59 +54,82 @@ namespace AKE_OperatorExtension
             }
             return false;
         }
-        private Trait HC_GetCurrentTrait()
+        private Trait HC_GetCurrentTrait(List<TraitAndDegree> traitset)
         {
             if (this.Pawn == null) return null;
-            if (TraitSets == null) return null;
-            for (int i = 0; i < this.Pawn.story.traits.allTraits.Count; i++)
+            if (traitset == null) return null;
+            for (int i = 0; i < traitset.Count; i++)
             {
-                for (int j = 0; i < TraitSets.Count; j++)
+                if (this.Pawn.story.traits.HasTrait(traitset[i].def))
                 {
-                    if (this.Pawn.story.traits.HasTrait(TraitSets[j].def))
-                    {
-                        index = j;
-                        return this.Pawn.story.traits.allTraits[i];
-                    }
+                    index = i;
+                    return this.Pawn.story.traits.GetTrait(traitset[i].def);
                 }
             }
+
             return null;
         }
-        private void HC_AddTrait()
+        private void HC_AddTrait(List<TraitAndDegree> traitset)
         {
-            if (this.Pawn == null) return;
-            if (TraitSets == null) return;
-            /*这部分今天懒得写先鸽了...记得使用index和UsedIndex计数检索
-            foreach (TraitAndDegree TraitAndDegree in this.TraitSets)
+            if (this.Pawn == null || traitset == null || traitset.Count == 0) return;
+            if (!usedIndices.TryGetValue(traitset, out var currentUsedIndex))
             {
-                TraitDegreeData data = TraitAndDegree.traitDef.degreeDatas[TraitAndDegree.degree];
+                currentUsedIndex = new HashSet<int>();
+                usedIndices[traitset] = currentUsedIndex;
+            }
+            if (currentUsedIndex.Count == traitset.Count)
+            {
+                currentUsedIndex.Clear();
+                index = 0;
+            }
+            bool traitAdded = false;
+            while (!traitAdded)
+            {
+                // 如果当前index已被使用，寻找下一个未使用的索引
+                if (currentUsedIndex.Contains(index))
+                {
+                    index = (index + 1) % traitset.Count; // 循环至下一个索引
+                    continue;
+                }
+                TraitAndDegree TraitAndDegree = traitset[index];
+                TraitDegreeData data = TraitAndDegree.def.degreeDatas[TraitAndDegree.degree];
                 if (data.skillGains != null)
                 {
-                    index++;
-                    this.Pawn.story.traits.GainTrait(new Trait(TraitAndDegree.traitDef, TraitAndDegree.degree));
+                    this.Pawn.story.traits.GainTrait(new Trait(TraitAndDegree.def, TraitAndDegree.degree));
+                    currentUsedIndex.Add(index);
+                    traitAdded = true;
                 }
-            }*/
+                index = (index + 1) % traitset.Count;
+            }
             hascurrentTrait = true;
         }
-        private void HC_RemoveTrait()
+        private void HC_RemoveTrait(List<TraitAndDegree> traitset)
         {
-            if (this.Pawn == null) return;
-            if (TraitSets == null) return;
+            if (this.Pawn == null || traitset == null || traitset.Count == 0) return;
             if (hascurrentTrait)
             {
-                this.Pawn.story.traits.RemoveTrait(HC_GetCurrentTrait());
+                this.Pawn.story.traits.RemoveTrait(HC_GetCurrentTrait(traitset));
                 hascurrentTrait = false;
             }
         }
         public override void CompPostTick(ref float severityAdjustment)
         {
             ++tick;
-            if (tick >= TimerInterval_days(10))
+            /*给冰淇淋用if (tick >= positiveTimerInterval_day && bingchilling)
             {
                 if (!HasOperatorTraitDef(GetOperatorTraitDef)) { parent.comps.Remove(this); return; }
-                HC_RemoveTrait();
+                if (HC_GetCurrentTrait(PositiveTraitSets) == null) { tick = 0; return; }
                 tick = 0;
-                HC_AddTrait();
-                index = 0;
+                HC_RemoveTrait(PositiveTraitSets);
+                HC_AddTrait(TraitSets);
+            }*/
+            if (tick >= TimerInterval_days)
+            {
+                if (!HasOperatorTraitDef(GetOperatorTraitDef)) { parent.comps.Remove(this); return; }
+                if (HC_GetCurrentTrait(TraitSets) == null) { tick = 0; return; }
+                tick = 0;
+                HC_RemoveTrait(TraitSets);
+                HC_AddTrait(TraitSets);
                 string translatedMessage = TranslatorFormattedStringExtensions.Translate("Phase1_SuccessMessage");
                 MoteMaker.ThrowText(this.Pawn.PositionHeld.ToVector3(), this.Pawn.MapHeld, translatedMessage, 2f);
             }

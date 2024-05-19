a863d2e64ddefad2e525d2f09b396d285da87ec4
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-05-19 20:32:13 +0800

    + 干员成长系统HCP(10%)

---
+++ AKE_OperatorExtension/HCP/HCP_GrowthSys.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AKE_OperatorExtension
+{
+    public class HCP_GrowthSys : HediffCompProperties
+    {
+        public HCP_GrowthSys()
+        {
+            this.compClass = typeof(HC_GrowthSys);
+        }
+    }
+    public class HC_GrowthSys : HediffComp
+    {
+        private int tick = 0;
+        public HCP_GrowthSys Props => (HCP_GrowthSys)this.props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            ++tick;
+            if (tick == 0)
+            {
+
+            }
+
+        }
+    }
+}

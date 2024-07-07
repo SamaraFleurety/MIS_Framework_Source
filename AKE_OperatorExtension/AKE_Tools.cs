d6c230216485feae4b338eff7ee24c7432a17066
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-07-07 18:25:55 +0800

    +水月HCP

---
+++ AKE_OperatorExtension/AKE_Tools.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AKE_OperatorExtension
+{
+    public static class AKE_Tools
+    {
+        internal static bool HasSkillTracker(this Pawn p)
+        {
+            return p != null && p.skills != null;
+        }
+        internal static bool HasTraitTracker(this Pawn p)
+        {
+            return p != null && p.story != null && p.story.traits != null;
+        }
+        internal static bool HasOperatorTraitDef(this Pawn p, string XMLdefName)
+        {
+            for (int i = 0; i < p.story.traits.allTraits.Count; i++)
+            {
+                if (p.story.traits.allTraits[i].def.defName == DefDatabase<TraitDef>.GetNamed(XMLdefName).defName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        internal static bool HasStoryTracker(this Pawn p)
+        {
+            return p != null && p.story != null;
+        }
+        internal static bool HasAbilityTracker(this Pawn p)
+        {
+            return p != null && p.abilities != null;
+        }
+    }
+}
---
+++ AKE_OperatorExtension/HCP/HCP_SkillUpbyTime.cs
@@ -0,0 +1,54 @@
+﻿using AK_DLL;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace AKE_OperatorExtension
+{
+    public class HCP_SkillUpbyTime : HediffCompProperties
+    {
+        public int interval = 1;
+        public string TargetTrait;
+        public HCP_SkillUpbyTime()
+        {
+            this.compClass = typeof(HC_SkillUpbyTime);
+        }
+    }
+
+    public class HC_SkillUpbyTime : HediffComp
+    {
+        private int tick = 0;
+        public HCP_SkillUpbyTime Props => (HCP_SkillUpbyTime)this.props;
+
+        public TimeToTick intervalUnit = TimeToTick.year;
+        private int Interval_year => this.Props.interval * (int)intervalUnit;
+        private string TargetTrait => this.Props.TargetTrait;
+        private List<SkillRecord> Skills => parent.pawn.skills.skills;
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            tick++;
+            if (tick >= (int)Interval_year)
+            {
+                tick = 0;
+                if (!parent.pawn.HasSkillTracker())
+                {
+                    return;
+                }
+                if (parent.pawn.HasOperatorTraitDef(TargetTrait))
+                {
+                    parent.comps.Remove(this);
+                    return;
+                }
+                foreach (SkillRecord Skill in Skills)
+                {
+                    Skill.Level += 1;
+                }
+            }
+        }
+    }
+}

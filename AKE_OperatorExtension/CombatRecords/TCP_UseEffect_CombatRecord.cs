2fe71df8f5e6da3a447c5115316c21ba4dada29a
MoonShine <ljhmyw@foxmail.com>
2024-08-01 15:01:46 +0800

    Update TCP_UseEffect_CombatRecord.cs

--- AKE_OperatorExtension/CombatRecords/TCP_UseEffect_CombatRecord.cs
+++ AKE_OperatorExtension/CombatRecords/TCP_UseEffect_CombatRecord.cs
@@ -9,24 +9,35 @@ using static HarmonyLib.Code;
 
 namespace AKE_OperatorExtension
 {
-    public class CP_UseEffect_CombatRecord : CompProperties_UseEffect
+    public class TCP_UseEffect_CombatRecord : CompProperties_UseEffect
     {
-        public SkillDef skill;
+        //public SkillDef skill;
 
         public float xpGainAmount = 1000f;
 
-        public CP_UseEffect_CombatRecord()
+        public TCP_UseEffect_CombatRecord()
         {
-            compClass = typeof(CompUseEffect_CombatRecord);
+            compClass = typeof(TC_UseEffect_CombatRecord);
         }
     }
-    public class CompUseEffect_CombatRecord : CompUseEffect
+    public class TC_UseEffect_CombatRecord : CompUseEffect
     {
-        public CP_UseEffect_CombatRecord Props => (CP_UseEffect_CombatRecord)props;
+        public TCP_UseEffect_CombatRecord Props => (TCP_UseEffect_CombatRecord)props;
+
+        private SkillDef skill;
+        public TC_UseEffect_CombatRecord()
+        {
+            skill = DefDatabase<SkillDef>.GetRandom();
+        }
 
         public override void DoEffect(Pawn user)
         {
-            user.skills.Learn(Props.skill, Props.xpGainAmount, direct: true);
+            user.skills.Learn(skill, Props.xpGainAmount, direct: true);
+        }
+
+        public override string TransformLabel(string label)
+        {
+            return base.TransformLabel(label) + $"({skill.label})";
         }
     }
 }

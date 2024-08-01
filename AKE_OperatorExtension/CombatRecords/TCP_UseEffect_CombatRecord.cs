5c5d135e8a97dcb2479863fa4232dbc67ee81503
rosen-roe <2826450573@qq.com>
2024-08-01 14:23:23 +0800

    初级作战记录

---
+++ AKE_OperatorExtension/CombatRecords/TCP_UseEffect_CombatRecord.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static HarmonyLib.Code;
+
+namespace AKE_OperatorExtension
+{
+    public class CP_UseEffect_CombatRecord : CompProperties_UseEffect
+    {
+        public SkillDef skill;
+
+        public float xpGainAmount = 1000f;
+
+        public CP_UseEffect_CombatRecord()
+        {
+            compClass = typeof(CompUseEffect_CombatRecord);
+        }
+    }
+    public class CompUseEffect_CombatRecord : CompUseEffect
+    {
+        public CP_UseEffect_CombatRecord Props => (CP_UseEffect_CombatRecord)props;
+
+        public override void DoEffect(Pawn user)
+        {
+            user.skills.Learn(Props.skill, Props.xpGainAmount, direct: true);
+        }
+    }
+}

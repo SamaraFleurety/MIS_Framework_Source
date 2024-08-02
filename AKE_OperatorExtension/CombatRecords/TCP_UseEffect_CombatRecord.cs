401f925b7a0aa7cc9b5e7e588b718941715fd01b
rosen-roe <2826450573@qq.com>
2024-08-02 23:37:17 +0800

    其他作战记录

--- AKE_OperatorExtension/CombatRecords/TCP_UseEffect_CombatRecord.cs
+++ AKE_OperatorExtension/CombatRecords/TCP_UseEffect_CombatRecord.cs
@@ -15,6 +15,8 @@ namespace AKE_OperatorExtension
 
         public float xpGainAmount = 1000f;
 
+        public List<int> rank = new List<int>{0,1,2,3};
+
         public TCP_UseEffect_CombatRecord()
         {
             compClass = typeof(TC_UseEffect_CombatRecord);
@@ -25,14 +27,47 @@ namespace AKE_OperatorExtension
         public TCP_UseEffect_CombatRecord Props => (TCP_UseEffect_CombatRecord)props;
 
         private SkillDef skill;
+
+        public Passion passion;
+
         public TC_UseEffect_CombatRecord()
         {
             skill = DefDatabase<SkillDef>.GetRandom();
         }
 
-        public override void DoEffect(Pawn user)
+        public override void DoEffect(Pawn user)//使用作战记录
         {
-            user.skills.Learn(skill, Props.xpGainAmount, direct: true);
+            float xpGain = Props.xpGainAmount;
+
+            foreach(int Rank in Props.rank)
+            {
+                switch (Rank)
+                {
+                    case 0://基础
+                        user.skills.Learn(DefDatabase<SkillDef>.GetRandom(), xpGain, direct: true);
+                        break;
+                    case 1://初级
+                        user.skills.Learn(skill, xpGain, direct: true);
+                        break;
+                    case 2://中级
+                        xpGain = 2000f;
+                        user.skills.Learn(skill, xpGain, direct: true);
+                        if (user.skills.PassionCount <= 5 && user.skills.GetSkill(skill).passion == Passion.None)
+                        {
+                            user.skills.GetSkill(skill).passion = Passion.Minor;
+                        }
+                        break;
+                    case 3://高级
+                        xpGain = 4000f;
+                        user.skills.Learn(skill, xpGain, direct: true);
+                        int numMajorSkills = user.skills.skills.Count(skillRecord => skillRecord.passion >= Passion.Major);
+                        if (numMajorSkills <= 3 && (user.skills.GetSkill(skill).passion == Passion.None || user.skills.GetSkill(skill).passion == Passion.Minor))
+                        {
+                            user.skills.GetSkill(skill).passion = Passion.Major;
+                        }
+                        break;
+                }
+            }
         }
 
         public override string TransformLabel(string label)
@@ -52,6 +87,12 @@ namespace AKE_OperatorExtension
                 return false;
             }
             return true;
+        }
+
+        public void ExposeData()
+        {
+            ExposeData();
+            Scribe_Defs.Look(ref skill, "skill");
         }
     }
 }

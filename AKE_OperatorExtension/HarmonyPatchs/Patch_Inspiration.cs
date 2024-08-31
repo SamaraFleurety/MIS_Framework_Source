20822793f86f2dbdc3fc3db9b0d7cefaf7d80932
MoonShine <ljhmyw@foxmail.com>
2024-08-31 14:09:09 +0800

    +null检定

--- AKE_OperatorExtension/HarmonyPatchs/Patch_Inspiration.cs
+++ AKE_OperatorExtension/HarmonyPatchs/Patch_Inspiration.cs
@@ -18,12 +18,12 @@ namespace AKE_OperatorExtension.HarmonyPatchs
     {
         private static void Postfix(Pawn victim, DamageInfo? dinfo, PawnDiedOrDownedThoughtsKind thoughtsKind, List<IndividualThoughtToAdd> outIndividualThoughts, List<ThoughtToAddToAll> outAllColonistsThoughts)
         {
-            if(dinfo == null || !dinfo.HasValue || dinfo.Value.Instigator == null) 
-            { 
+            if (dinfo == null || !dinfo.HasValue || dinfo.Value.Instigator == null)
+            {
                 return;
             }
             Pawn pawn = (Pawn)dinfo.Value.Instigator;
-            if(pawn == null) { return; }
+            if (pawn == null) { return; }
             if (pawn.story.traits.HasTrait(TraitDef.Named("AK_Trait_SpecterUnchainedSecond")))
             {
                 float inspirationChance = 0.5f;
@@ -43,6 +43,7 @@ namespace AKE_OperatorExtension.HarmonyPatchs
         public static bool Prefix(ref InspirationDef __result, InspirationHandler __instance)
         {
             Pawn pawn = __instance.pawn;
+            if (pawn.story == null || pawn.story.traits == null) return HarmonyPrefixRet.keepOriginal;
             if (pawn.story.traits.HasTrait(TraitDef.Named("AK_Trait_SpecterUnchainedThird")))
             {
                 __result = DefDatabase<InspirationDef>.AllDefsListForReading.Find(def => def == InspirationDefOf.Inspired_Creativity);
@@ -59,7 +60,10 @@ namespace AKE_OperatorExtension.HarmonyPatchs
         [HarmonyPostfix]
         public static void Postfix(Pawn pawn, Thing ingested)
         {
-            if (ingested.def.GetCompProperties<CompProperties_Drug>().chemical == ChemicalDefOf.Alcohol && pawn.story.traits.HasTrait(TraitDef.Named("AK_Trait_Blaze")))
+            if (pawn == null || ingested == null || pawn.story == null || pawn.story.traits == null) return;
+            CompProperties_Drug tcp = ingested.def.GetCompProperties<CompProperties_Drug>();
+            if (tcp == null) return;
+            if (tcp.chemical == ChemicalDefOf.Alcohol && pawn.story.traits.HasTrait(TraitDef.Named("AK_Trait_Blaze")))
             {
                 InspirationDef randomInspiration = DefDatabase<InspirationDef>.AllDefsListForReading.RandomElement();
                 pawn.mindState.inspirationHandler.TryStartInspiration(randomInspiration);

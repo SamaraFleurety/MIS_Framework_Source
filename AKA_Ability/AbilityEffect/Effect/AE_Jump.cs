be22d4511e12d92dc0bb347ce1941cea1520d2f5
Paluto <85861331+Paluto42@users.noreply.github.com>
2025-09-17 18:28:00 +0800

    Update AE_Jump.cs

--- AKA_Ability/AbilityEffect/Effect/AE_Jump.cs
+++ AKA_Ability/AbilityEffect/Effect/AE_Jump.cs
@@ -1,11 +1,26 @@
-﻿using RimWorld;
+﻿using AKA_Ability.AbilityEffect;
+using RimWorld;
+using System.Collections.Generic;
 using Verse;
 
 namespace AKA_Ability
 {
-    public class AE_Jump : AbilityEffectBase
+    public class AE_Jump : AbilityEffectBase, ITargetingValidator
     {
         public VerbProperties verbJump;
+
+        public bool TargetingValidator(TargetInfo info)
+        {
+            List<Thing> list = info.Map.thingGrid.ThingsListAtFast(info.Cell);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].def.passability == Traversability.Impassable)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
         protected override bool DoEffect(AKAbility_Base caster, LocalTargetInfo target)
         {
